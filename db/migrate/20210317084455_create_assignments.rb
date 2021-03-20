31f6fa230b6cee5954a09c7a90473ce588fe4540
yiran123 <yyyiran@hotmail.com>
2021-03-20 01:53:21 -0700

    migration

--- db/migrate/20210317084455_create_assignments.rb
+++ db/migrate/20210317084455_create_assignments.rb
@@ -1,8 +1,8 @@
 class CreateAssignments < ActiveRecord::Migration[6.1]
   def change
     create_table :assignments do |t|
-      t.references :guest, null: false, foreign_key: { to_table: :guest }
-      t.references :room, null: false, foreign_key: { to_table: :room }
+      t.references :guest, null: false, foreign_key: { to_table: :guests }
+      t.references :room, null: false, foreign_key: { to_table: :rooms }
 
       t.timestamps
     end
